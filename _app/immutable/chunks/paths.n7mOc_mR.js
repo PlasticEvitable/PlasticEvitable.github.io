a0d5aca0c24d858072b4b17def0489a3e6077297
Shramee Srivastav <shramee.srivastav@gmail.com>
2024-05-19 22:56:32 +0530

    Updates

--- _app/immutable/chunks/paths.n7mOc_mR.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_lcnz3o)==null?void 0:s.base)??"/landing";var a;const l=((a=globalThis.__sveltekit_lcnz3o)==null?void 0:a.assets)??e;export{l as a,e as b};
---
+++ _app/immutable/entry/start.D1cow6j6.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.WHU5Bgt6.js";export{t as start};
